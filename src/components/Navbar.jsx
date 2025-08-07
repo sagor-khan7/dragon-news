8b6bea2b87a3c37f5646713f8c39976a64808c80
sagor-khan7 <sksagor791@gmail.com>
2025-08-07 15:11:59 +0600

    created navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import userIcon from "../assets/user.png";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <div></div>
+      <div className="space-x-5">
+        <Link to="/" className="text-lg text-[#706F6F]">
+          Home
+        </Link>
+        <Link to="/about" className="text-lg text-[#706F6F]">
+          About
+        </Link>
+        <Link to="/career" className="text-lg text-[#706F6F]">
+          Career
+        </Link>
+      </div>
+      <div className="flex space-x-3 items-center">
+        <div>
+          <img src={userIcon} alt="user profile icon" />
+        </div>
+        <button className="btn rounded-none bg-[#403F3F] text-white font-semibold text-xl">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
   return (
@@ -10,6 +11,9 @@ const HomeLayout = () => {
           <LatestNews />
         </section>
       </header>
+      <nav className="w-11/12 mx-auto py-6">
+        <Navbar />
+      </nav>
     </div>
   );
 };

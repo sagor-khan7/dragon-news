77b6d62f8ba31ea444c5c4bd6a4b53c07cfeacdf
sagor-khan7 <sksagor791@gmail.com>
2025-08-08 02:06:41 +0600

    removing comment

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -2,28 +2,6 @@ import { Link, useLoaderData } from "react-router-dom";
 import Header from "../components/Header";
 import RightNavbar from "../components/layout-component/RightNavbar";
 
-// {
-//     "_id": "f69a695f037cd9484cecaea37ca71011",
-//     "others_info": {
-//         "is_todays_pick": false,
-//         "is_trending": true
-//     },
-//     "category_id": "01",
-//     "rating": {
-//         "number": 4.8,
-//         "badge": "Excellent"
-//     },
-//     "total_view": 400,
-//     "title": "Tucker Carlson Swears Vladimir Putin Is Winning The War In Ukraine",
-//     "author": {
-//         "name": "Jennifer Wood",
-//         "published_date": "2022-08-30 16:38:45",
-//         "img": "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
-//     },
-//     "thumbnail_url": "https://i.ibb.co/k8XBcMX/unsplash-Eh-Tc-C9s-YXsw-15.png",
-//     "image_url": "https://i.ibb.co/BZZ5WKv/unsplash-Eh-Tc-C9s-YXsw-4.png",
-//     "details": "Tucker Carlson has rarely met a dictator's ass he didn't want to kiss, and Vladimir Putin is at the very top of that puckering up list. The Fox News host is something of a folk hero in Russia: Because of the pro-Putin propaganda he so often spews, the Kremlin has encouraged Russia's state TV to air as much of Tucker's face as possible — and they're certain to love his latest rant about how Putin is winning the war in Ukraine, which does not seem to be the case. But on Fox News, what Tucker says goes."
-// }
 const NewsDetails = () => {
   const { data } = useLoaderData();
   const news = data[0];
